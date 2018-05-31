8ec5f0aa56fcdb0ef2308aa6c613f6115f72ad59
SevenTrio <36824327+SevenTrio@users.noreply.github.com>
2018-05-31 21:40:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ client.on('message', (message) => {
     };
     
       for (var i = 0, sum = 0; i < x.length; sum += x[i++]);  
-      y = sum % 27;
+      y = sum % 20;
       message.channel.send('8'+'='.repeat(y)+'D');
     };
 
