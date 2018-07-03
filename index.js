b9e47159a33c2263012699debd3db18c328da9e9
SevenTrio <36824327+SevenTrio@users.noreply.github.com>
2018-07-03 23:14:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ client.on('message', (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
-  if(message.channel.id === '424964715547197455' && !['lick', 'llick', 'yoba'].includes(command) && !['218656629720219658','218719595618500608','218562543185035266'].includes(message.author.id)) return;
+  if(message.channel.id === '424964715547197455' && !['lick', 'llick', 'yoba', 'react'].includes(command) && !['218656629720219658','218719595618500608','218562543185035266'].includes(message.author.id)) return;
   
   if(!['ping', 'help', 'test'].includes(command) && message.channel.type === 'dm') return message.channel.send('Извините, но данная команда не доступна в личных сообщениях с ботом.');
 
